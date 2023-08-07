ad6aabb0a9c169dc2313771a599fbddbebf35224
cuiding <cuiding@yeez.tech>
2023-08-07 16:30:22 +0800

    前端学习

--- TS学习/demo.js
+++ TS学习/demo.js
@@ -1,12 +1,24 @@
-function createSquare(config) {
-    var newSquare = { color: "white", area: 100 };
-    if (config.color) {
-        // Error: Property 'clor' does not exist on type 'SquareConfig'
-        newSquare.color = config.color;
-    }
-    if (config.width) {
-        newSquare.area = config.width * config.width;
-    }
-    return newSquare;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Greeter(greeting) {
+    return function (target) {
+        target.prototype.greet = function () {
+            console.log(greeting);
+        };
+    };
 }
-var mySquare = createSquare({ color: "black" });
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+        // 内部实现
+    }
+    Greeting = __decorate([
+        Greeter("Hello TS!")
+    ], Greeting);
+    return Greeting;
+}());
+var myGreeting = new Greeting();
+myGreeting.greet(); // console output: 'Hello TS!';
--- TS学习/demo.ts
+++ TS学习/demo.ts
@@ -1,18 +1,16 @@
-interface SquareConfig {
-    color?: string;
-    width?: number;
+
+function Greeter(target: Function): void {
+  target.prototype.greet = function (): void {
+    console.log("Hello Semlinker!");
+  };
+}
+
+@Greeter
+class Greeting {
+  constructor() {
+    // 内部实现
   }
-  
-  function createSquare(config: SquareConfig): { color: string; area: number } {
-    let newSquare = { color: "white", area: 100 };
-    if (config.color) {
-      // Error: Property 'clor' does not exist on type 'SquareConfig'
-      newSquare.color = config.color;
-    }
-    if (config.width) {
-      newSquare.area = config.width * config.width;
-    }
-    return newSquare;
-  }
-  
-  // let mySquare = createSquare({ color: "black" });
+}
+
+let myGreeting = new Greeting();
+(myGreeting as any).greet(); // console output: 'Hello Semlinker!';
